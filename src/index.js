9e38c235bff1d5870c56708d91e5812f4c9d1cc6
Miden0 <midenoprog@gmail.com>
2022-01-09 00:55:27 -0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const Form = () => {
   const [titulo, setTitulo] = useState('');
+  const [show, setShow] = useState(false);
+  const tituloRef = useRef();
+
+  useEffect(() => {
+    if (show) {
+      tituloRef.current.focus();
+    }
+  });
 
   return (
-    <form>
-      <label htmlFor="title"> Titulo </label>
-      <input
-        value={titulo}
-        type="text"
-        id="title"
-        onChange={(ev) => setTitulo(ev.target.value)}
-      />
-    </form>
+    <div>
+      <button onClick={() => setShow(!show)}>
+        {(show && 'hide') || 'show'}
+      </button>
+      {show && (
+        <form>
+          <label htmlFor="title"> Titulo </label>
+          <input
+            ref={tituloRef}
+            value={titulo}
+            type="text"
+            id="title"
+            onChange={(ev) => setTitulo(ev.target.value)}
+          />
+        </form>
+      )}
+    </div>
   );
 };
 
